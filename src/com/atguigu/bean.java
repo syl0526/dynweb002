de8d5f9d6be349ddb0cbffd6570c3d48e9c077d3
司永乐 <siyongle0725@163.com>
2020-07-01 16:41:08 +0800

    Initial commit

---
+++ src/com/atguigu/bean.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author S Y L
+ * @create 2020-07-01 16:33
+ */
+public class bean {
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
